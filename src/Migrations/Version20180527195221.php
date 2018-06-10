c722715e5190a06e66a313c4e64d8e2f4e03c21a
Pi-Bouf <pierre.bouffier05@gmail.com>
2018-06-10 19:19:57 +0200

    Changing first revision

--- src/Migrations/Version20180527195221.php
+++ src/Migrations/Version20180527195221.php
@@ -9,11 +9,11 @@ final class Version20180527195221 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
-        $this->addSql('INSERT INTO versions VALUES(null, "BETA-20180527", now(), 1)');
+        $this->addSql('INSERT INTO versions VALUES(null, "BETA-001", now(), 1)');
     }
 
     public function down(Schema $schema) : void
     {
-        $this->addSql('DELETE FROM versions WHERE version_number= "BETA-20180527"');
+        $this->addSql('DELETE FROM versions WHERE version_number= "BETA-001"');
     }
 }
